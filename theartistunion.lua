47fa2778564945a7f14a81d6c062f003fc631c96
luckcolors <luckcolorsgoo@gmail.com>
2019-06-26 21:54:31 +0200

    First commit

---
+++ theartistunion.lua
@@ -0,0 +1,62 @@
+local url_count = 0
+local tries = 0
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  return false
+end
+
+wget.callbacks.get_urls = function(file, url, is_css, iri)
+  local urls = {}
+  if string.match(url, "^https?://theartistunion\.com/api/v3/tracks/(.{6})\.json$") then
+    local newUrl = string.gsub(url, ".json", "/", 1)
+    newUrl.concat("related.json")
+    table.insert(urls, {url=newurl})
+    return urls
+  end
+  return urls
+end
+
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  status_code = http_stat["statcode"]
+
+  url_count = url_count + 1
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. "  \n")
+  io.stdout:flush()
+
+  if status_code == 401 then
+    return wget.actions.ABORT
+  end
+  
+  if status_code >= 500 or
+    (status_code >= 400 and status_code ~= 403 and status_code ~= 404 and status_code ~= 410) or
+    status_code == 0 then
+    io.stdout:write("Server returned "..http_stat.statcode.." ("..err.."). Sleeping.\n")
+    io.stdout:flush()
+    if status_code == 429 then
+      os.execute("sleep 3600")
+      return wget.actions.ABORT
+    else
+      os.execute("sleep 1")
+    end
+    tries = tries + 1
+    if tries >= 5 then
+      io.stdout:write("\nI give up...\n")
+      io.stdout:flush()
+      tries = 0
+      return wget.actions.ABORT
+    else
+      return wget.actions.CONTINUE
+    end
+  end
+
+  tries = 0
+
+  local sleep_time = 2
+
+  if sleep_time > 0.001 then
+    os.execute("sleep " .. sleep_time)
+  end
+
+  return wget.actions.NOTHING
+end
+
