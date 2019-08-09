9600b60ef0f7375d91cf54b3e7bfa9883da2e5fc
Matt Iggo <matt@iggo.co.uk>
2019-08-09 15:00:51 +0100

    Updated original_file insertion to match changes to original_files json
    Even though the original_files in the base item.json are valid, there are some other files. Updated to capture those also.

--- theartistunion.lua
+++ theartistunion.lua
@@ -153,6 +153,20 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
     table.insert(urls, { url=a .. "original_files" .. b .. ".wav" })
     table.insert(urls, { url=a .. "original_files" .. b .. ".mp3" })
     -- IMPORTANT make sure to set number of fails possible in httploop_result
+  elseif string.match(url, "^https?://[^%.]+%.cloudfront%.net/tracks/original_files/.+%.wav%?[0-9]+$") then
+    local a, b = string.match(url, "^(https?://[^%.]+%.cloudfront%.net/tracks/)original_files(/.+)%.wav%?[0-9]+$")
+    table.insert(urls, { url=a .. "original_files" .. b .. ".wav" })
+    table.insert(urls, { url=a .. "original_files" .. b .. ".mp3" })
+    -- IMPORTANT make sure to set number of fails possible in httploop_result
+  elseif string.match(url, "^https?://[^%.]+%.cloudfront%.net/tracks/original_files/.+%.mp3%?[0-9]+$") then
+    local a, b = string.match(url, "^(https?://[^%.]+%.cloudfront%.net/tracks/)original_files(/.+)%.mp3%?[0-9]+$")
+    table.insert(urls, { url=a .. "original_files" .. b .. ".wav" })
+    table.insert(urls, { url=a .. "original_files" .. b .. ".mp3" })
+    -- IMPORTANT make sure to set number of fails possible in httploop_result
+  elseif string.match(url, "^https?://content%.theartistunion%.com/tracks/audio/stream_encode/.+%.mp3$") then
+    local a, b = string.match(url, "^(https?://content%.theartistunion%.com/tracks/audio/)stream_encode(/.+)%.mp3$")
+    table.insert(urls, { url=a .. ":original" .. b .. ".wav" })
+    table.insert(urls, { url=a .. ":original" .. b .. ".mp3" })
   elseif string.match(url, "^https?://content%.theartistunion%.com/tracks/audio/stream_encode/[^%.]+%.mp3$") then
     local a, b = string.match(url, "^(https?://content%.theartistunion%.com/tracks/audio/)stream_encode(/[^%.]+)%.mp3$")
     table.insert(urls, { url=a .. ":original" .. b .. ".wav" })
