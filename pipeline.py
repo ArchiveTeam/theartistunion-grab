cf674d28769f722a006d80f7ea6d4b224c882bb7
arkiver <arkiver@protonmail.com>
2020-08-19 23:22:18 -0400

    Version 20200820.01. Use trackerproxy.

--- pipeline.py
+++ pipeline.py
@@ -51,10 +51,10 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20200805.01'
+VERSION = '20200820.01'
 USER_AGENT = 'Archive Team'
 TRACKER_ID = 'theartistunion'
-TRACKER_HOST = 'tracker.archiveteam.org'
+TRACKER_HOST = 'trackerproxy.meo.ws'
 
 
 ###########################################################################
